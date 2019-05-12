ba42dfa608adb415ee011346862c068a0369c08d
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 17:15:10 +0300

    adds a test user class
    The test user class is setup and the first test runs

---
+++ user_test.py
@@ -0,0 +1,13 @@
+import unittest
+from user import User
+
+class User_test(unittest.TestCase):
+    def setUp(self):
+        self.new_user = User('harry','kk',[])
+    
+
+
+    
+
+    if __name__ == "__main__":
+        unittest.TestCase()
