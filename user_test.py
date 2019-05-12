8e1224dcdc55d992d2bdd651a0b165f8e9e2a47d
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 17:16:51 +0300

    adds a test to test saving user
    this tests if the created user can be saved

--- user_test.py
+++ user_test.py
@@ -5,6 +5,16 @@ class User_test(unittest.TestCase):
     def setUp(self):
         self.new_user = User('harry','kk',[])
     
+        def tearDown(self):
+        User.users_list = {}
+
+
+    def test_save_user(self):
+        self.new_user.make_user_dict()
+        self.assertTrue(len(User.users_list),1)
+
+    
+
 
 
     
