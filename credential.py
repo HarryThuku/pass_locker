b0392926fe0e4813bdfec78f9d86cdc749faaf92
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 12:34:45 +0300

    adds class variables to the credential class.

---
+++ credential.py
@@ -0,0 +1,10 @@
+class Credential:
+    '''
+    '''
+
+    def __init__( self, account_name, account_url, account_pass ):
+        '''
+        '''
+        self.account_name = account_name
+        self.account_url = account_url
+        self.account_pass = account_pass
