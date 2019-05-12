42085e4481da0a24bf6fd0c2977ab63ba989b749
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 12:37:36 +0300

    adds a method to create a user dictionary instance.

--- user.py
+++ user.py
@@ -12,4 +12,14 @@ class User:
         self.credentials = []
 
 
+    def make_user_dict(self):
+        '''
+        '''
+
+        user = { }
+        user[self.user_name] = dict( user_name = self.user_name, password = self.password, credentials = [] )
+        print('______________________ Successfully created an account for user {}. Do you want to save your progress? ______________________\n'.format(self.user_name))
+        return user
+
+
     
