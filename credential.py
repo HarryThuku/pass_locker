ec25d3802d9194aa169f405e32883606eb8ef1a1
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 12:45:38 +0300

    edits class variables to prevent variable naming conflict

--- credential.py
+++ credential.py
@@ -5,6 +5,6 @@ class Credential:
     def __init__( self, account_name, account_url, account_pass ):
         '''
         '''
-        self.account_name = account_name
-        self.account_url = account_url
-        self.account_pass = account_pass
+        self.cred_name = account_name
+        self.cred_url = account_url
+        self.cred_pass = account_pass
