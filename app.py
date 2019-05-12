b66799cfe68e3b30f8052e1e03285497844ec263
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 16:27:20 +0300

    adds a login method
    At this stage, the login method is able to find the users' account and ready it for login.

--- app.py
+++ app.py
@@ -8,3 +8,13 @@ def emptyChecker(*args):
     results = all([item!='' for item in args])
     return results
 
+def login():
+    '''
+    '''
+    main_user = User('','',[])
+    while True:
+        print('\t\tlogin page')
+        print('__________________________________  __________________________________')
+        username = input('Enter your username : ')
+        account = main_user.find_user_by_uname(username)
+        print(account)
