6fb32b7c94195f2a58158460bed80c3dfa55bacc
HarryThuku <harsygeorgee@gmail.com>
2019-05-12 12:36:05 +0300

    adds the user class.
    the user class contains variables that shape how data will be stored
    
    it is in this instance where it is initialized

---
+++ user.py
@@ -0,0 +1,15 @@
+class User:
+    users_list = {}
+    '''
+    '''
+
+    def __init__( self, user_name, password, credentials ):
+        '''
+        '''
+
+        self.user_name = user_name
+        self.password = password
+        self.credentials = []
+
+
+    
